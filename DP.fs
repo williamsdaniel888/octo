e716100436fb58bcb5a0db8c38f418d08b48718f
Dan <williamsdaniel888@gmail.com>
2018-02-12 21:03:53 +0000

    Split initProjectLexer

---
+++ DP.fs
@@ -0,0 +1,67 @@
+module DP =
+    open CommonData
+    open CommonLex
+
+    // change these types as required
+
+    /// instruction (dummy: must change)
+    type Instr =  {DPDummy: Unit}
+
+    /// parse error (dummy, but will do)
+    type ErrInstr = string
+
+    /// sample specification for set of instructions
+    /// very incomplete!
+    let dPSpec = {
+        InstrC = DP
+        Roots = ["ADD";"SUB"]
+        Suffixes = [""; "S"]
+    }
+
+    /// map of all possible opcodes recognised
+    let opCodes = opCodeExpand dPSpec
+
+    /// main function to parse a line of assembler
+    /// ls contains the line input
+    /// and other state needed to generate output
+    /// the result is None if the opcode does not match
+    /// otherwise it is Ok Parse or Error (parse error string)
+    let parse (ls: LineData) : Result<Parse<Instr>,string> option =
+        let parse' (instrC, (root,suffix,pCond)) =
+
+            let (WA la) = ls.LoadAddr // address this instruction is loaded into memory
+            // this does the real work of parsing
+            // dummy return for now
+            Ok { 
+                // Normal (non-error) return from result monad
+                // This is the instruction determined from opcode, suffix and parsing
+                // the operands. Not done in the sample.
+                // Note the record type returned must be written by the module author.
+                PInstr={DPDummy=()}; 
+
+
+                // This is normally the line label as contained in
+                // ls together with the label's value which is normally
+                // ls.LoadAddr. Some type conversion is needed since the
+                // label value is a number and not necessarily a word address
+                // it does not have to be div by 4, though it usually is
+                PLabel = ls.Label |> Option.map (fun lab -> lab, la) ; 
+
+
+                // this is the number of bytes taken by the instruction
+                // word loaded into memory. For arm instructions it is always 4 bytes. 
+                // For data definition DCD etc it is variable.
+                //  For EQU (which does not affect memory) it is 0
+                PSize = 4u; 
+
+                // the instruction condition is detected in the opcode and opCodeExpand                 
+                // has already calculated condition already in the opcode map.
+                // this part never changes
+                PCond = pCond 
+                }
+        Map.tryFind ls.OpCode opCodes // lookup opcode to see if it is known
+        |> Option.map parse' // if unknown keep none, if known parse it.
+
+
+    /// Parse Active Pattern used by top-level code
+    let (|IMatch|_|) = parse
